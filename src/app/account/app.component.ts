88bc56aaa83f0864bd423b0724d745331856634a
JollySL <jolly.lengkono@gmail.com>
2017-10-11 13:19:27 +0700

    adjust path after moved app.component

--- src/app/account/app.component.ts
+++ src/app/account/app.component.ts
@@ -6,8 +6,8 @@ import {AccountService} from './account.services';
 
 @Component({
   selector: 'my-app',
-  templateUrl: 'app/app.component.html',
-  styleUrls:['app/app.component.css']
+  templateUrl: 'app/account/app.component.html',
+  styleUrls:['app/account/app.component.css']
 })
 
 export class AppComponent  {
